18d611b872ea4675a9fb17d4eea56862d2c332c4
neewb2022 <tmp_mail2023@proton.me>
2022-11-16 11:14:51 +0300

    update

--- ModelPriceComparison/build_mpc.bat
+++ ModelPriceComparison/build_mpc.bat
@@ -1,1 +1,2 @@
-%CD%\env1\Scripts\pyinstaller.exe --onefile --version-file=file_version_info.txt -i icon.png --workpath ..\build --name mpricecomp main.py
+%CD%\env1\Scripts\create-version-file.exe file_version_info.yml --outfile file_version_info.txt
+%CD%\env1\Scripts\pyinstaller.exe --onefile --version-file=file_version_info.txt -i icon.png --workpath %temp% --name mpricecomp main.py
